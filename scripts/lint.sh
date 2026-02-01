a202a3b71eaa9b980c8b5e48285e986b1c876c4c
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2026-02-01 17:06:47 +0100

    scripts/lint: add optional dir arg, add usage error message

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+if [[ "$#" -eq 1 ]]; then
+    cd "$1"
+elif [[ "$#" -ne 0 ]]; then
+    echo "error: usage: ./scripts/lint.sh [dir]" >&2
+    exit 1
+fi
+
 # lint debs
 shopt -s nullglob
 for f in *.dsc *.changes *.deb; do
